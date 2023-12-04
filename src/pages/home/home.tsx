90bbb96e9674c826b1677a8f398338ff092a72a8
Trung <trungnguyen.02082000@gmail.com>
2023-12-04 16:35:40 +0700

    cài tailwwind

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Home() {
     return (
-        <div>Home</div>
+        <div>
+            <p className='text-red-500 text-8xl'>Home</p>
+        </div>
     )
 }
 
