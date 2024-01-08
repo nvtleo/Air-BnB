efda9ba96f2e086f2f59390a0746bd9775a32aeb
Trung <trungnguyen.02082000@gmail.com>
2024-01-08 10:34:50 +0700

    xong giao diện của đăng nhập đăng ký

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,9 +1,61 @@
-import React from 'react'
-
+// import React from "react";
+import { Input, Row, Col } from "antd";
+import { useFormik } from "formik";
+import * as Y from "yup";
+import { signIn } from "../../service/register.service";
+// import { saveLocal } from "../../util/index";
+// import { ACCESS_TOKEN } from "../../const/index";
+// import { useDispatch } from "react-redux";
+// import { loginSuccess } from "../../redux/userSlice";
+// import { useAppSelector } from "../../redux/hook";
+import { useNavigate } from "react-router-dom";
+const validationSchema = Y.object({
+    email: Y.string().email().required(),
+    password: Y.string().required(),
+});
 function Login() {
+    const navigate = useNavigate();
+    // const dispatch = useDispatch();
+    const { handleSubmit, getFieldProps } = useFormik({
+        initialValues: {
+            email: "",
+            password: "",
+        },
+        validationSchema,
+        onSubmit: (values) => {
+            console.log(values);
+            signIn(values).then((resp) => {
+                alert("Đăng nhập thành công")
+                navigate("/");
+                // saveLocal(ACCESS_TOKEN, resp.accessToken);
+                // dispatch(
+                //     loginSuccess({
+                //         email: resp.email,
+                //     }),
+                // );
+            });
+        },
+    });
     return (
-        <div>Login</div>
-    )
+        <form
+            onSubmit={handleSubmit} className="px-16 py-16">
+            <Row>
+                <Col lg={24}>
+                    <label>Email</label>
+                    <Input {...getFieldProps("email")} />
+                </Col>
+                <Col lg={24}>
+                    <label>Pass Word</label>
+                    <Input {...getFieldProps("password")} />
+                </Col>
+            </Row>
+            <button
+                type="submit"
+                className="bg-lime-600 text-white px-4 py-2 rounded-2xl mt-4"
+            >
+                Login
+            </button>
+        </form>
+    );
 }
-
-export default Login
+export default Login;
